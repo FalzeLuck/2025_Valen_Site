4124c5e59a2587d229c4aea8280d4db3794a426c
FalzeLuck <film111ch@gmail.com>
2025-02-13 16:08:34 +0700

    Change rose 1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("valentineButton").addEventListener("click", function() 
     const roseContainer = document.getElementById("roseContainer");
     for (let i = 0; i < 10; i++) {
         let rose = document.createElement("img");
-        rose.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Rose_icon.svg/512px-Rose_icon.svg.png";
+        rose.src = "https://static.vecteezy.com/system/resources/previews/003/346/596/non_2x/cute-rose-natural-isolated-icon-free-vector.jpg";
         rose.className = "rose";
 
         // กำหนดตำแหน่งแบบสุ่ม
