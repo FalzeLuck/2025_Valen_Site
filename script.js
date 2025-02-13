ff932a5ef53165da4f970fcd4fbd52cc952441e6
FalzeLuck <film111ch@gmail.com>
2025-02-13 16:17:32 +0700

    Change All

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.getElementById("valentineButton").addEventListener("click", function() {
+    // Scene Change
+    document.getElementById("scene1").classList.add("hidden");
+    document.getElementById("scene2").classList.remove("hidden");
+    document.body.style.backgroundColor = "#ffb6c1"; // Change background color
+
+    // Show Roses
     const roseContainer = document.getElementById("roseContainer");
     for (let i = 0; i < 10; i++) {
         let rose = document.createElement("img");
-        rose.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Red_Rose_Bouquet.svg/512px-Red_Rose_Bouquet.svg.png"; // New rose image
+        rose.src = "https://www.misskatecuttables.com/uploads/shopping_cart/9755/large_rose-flower-group2.png"; // New rose image
         rose.className = "rose";
 
         // Random position
